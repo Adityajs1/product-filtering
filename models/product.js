0fee295042e9e9a1e6814776679f866044241b9a
Adityajs1 <aditya96937@gmail.com>
2026-03-12 22:21:21 +0530

    defining the products schema

---
+++ models/product.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+    name :{
+        type : Number,
+        required :[true,'product name must be provide' ]
+    },
+    price:{
+        type: String,
+        required :[true,'product price must be provide' ]
+    },
+    featured:{
+        type: Boolean,
+        default : false
+    },
+    rating:{
+        type : Number,
+        default : 4.0
+    },
+    createdAt:{
+        type : Date,
+        default : Date.now()
+    },
+    company: {
+        type : String,
+        enum:{
+            values: ['ikea', 'liddy', 'caressa', 'marcos']
+        }
+    }
+})
